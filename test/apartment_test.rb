144c95f655edb0edf764898f8fb4651e54b6692c
Jack Gould <johnhilsmangould@gmail.com>
2020-03-31 10:01:37 -0600

    add add renter test method

--- test/apartment_test.rb
+++ test/apartment_test.rb
@@ -1,18 +1,19 @@
 require 'minitest/autorun'
 require 'minitest/pride'
+require './lib/apartment'
 require 'pry';
 
 
 class ApartmentTest < Minitest::Test
   def test_it_exists
-    skip
+
     apartment = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
 
     assert_instance_of Apartment, apartment
   end
 
   def test_it_has_readable_attributes
-    skip
+
     apartment = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
 
     assert_equal "A1", apartment.number
@@ -20,6 +21,13 @@ class ApartmentTest < Minitest::Test
     assert_equal 1 , apartment.bathrooms
     assert_equal 1 , apartment.bedrooms
 
+  end
+
+  def test_it_can_add_renters
+
+    apartment = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
+    
+    assert_equal renter1, apartment.add_renter(renter1)
   end
 
 end
