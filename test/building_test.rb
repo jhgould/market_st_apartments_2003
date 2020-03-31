06791356d5cc385ea90d376b7a03bf8edbc5df2e
Jack Gould <johnhilsmangould@gmail.com>
2020-03-31 10:20:40 -0600

    add add renters test method

---
+++ test/building_test.rb
@@ -0,0 +1,43 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/apartment'
+require './lib/renter'
+require './lib/building'
+require 'pry';
+
+
+class BuildingTest < Minitest::Test
+  def test_it_exists
+
+    building = Building.new
+
+    assert_instance_of Building, building
+  end
+
+  def test_it_has_readable_attributes
+
+    building = Building.new
+
+    assert_equal [], building.units
+    assert_equal [], building.renters
+  end
+
+    def test_it_can_add_units
+       building = Building.new
+       unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
+
+      assert_equal [unit1], building.add_unit(unit1)
+    end
+
+    def test_it_can_add_renters
+      building = Building.new
+      renter1 = Renter.new("Jack")
+      unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
+
+      assert_equal [renter1], building.add_renter(renter1)
+    end
+
+
+
+
+end
