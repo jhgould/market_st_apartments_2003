6859bd37078010282e6e015995cc8fae6371e99a
Jack Gould <johnhilsmangould@gmail.com>
2020-04-01 10:17:40 -0600

    corrected tests

--- test/building_test.rb
+++ test/building_test.rb
@@ -29,20 +29,32 @@ class BuildingTest < Minitest::Test
       assert_equal [unit1], building.add_unit(unit1)
     end
 
-    def test_it_can_add_renters
+    def test_can_add_units_and_renters
+        building = Building.new
+        unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
+        unit2 = Apartment.new({number: "B2", monthly_rent: 999, bathrooms: 2, bedrooms: 2})
+        building.add_unit(unit1)
+        building.add_unit(unit2)
+        renter1 = Renter.new("Aurora")
+        assert_equal [unit1, unit2], building.units
+        unit1.add_renter(renter1)
+        building.renters
+        assert_equal ["Aurora"], building.renters
+      end
 
-      building = Building.new
-      renter1 = Renter.new("Aurora")
-      unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
-      building.add_unit(unit1)
-      unit1.add_renter(renter1)
-      building.add_renter(renter1)
-
-
-
-      assert_equal ["Aurora"], building.add_renter
-    end
-
+      def test_can_average_rent
+    building = Building.new
+    unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
+    unit2 = Apartment.new({number: "B2", monthly_rent: 999, bathrooms: 2, bedrooms: 2})
+    building.add_unit(unit1)
+    building.add_unit(unit2)
+    renter1 = Renter.new("Aurora")
+    assert_equal [unit1, unit2], building.units
+    unit1.add_renter(renter1)
+    renter2 = Renter.new("Tim")
+    unit1.add_renter(renter2)
+    assert_equal 1099.5, building.average_rent
+  end
 
 
 
