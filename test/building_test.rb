a31473f4d4f093fb4380bb1655d6958f1b129595
Jack Gould <johnhilsmangould@gmail.com>
2020-04-01 10:47:11 -0600

    add average_rent test method

--- test/building_test.rb
+++ test/building_test.rb
@@ -7,29 +7,29 @@ require 'pry';
 
 
 class BuildingTest < Minitest::Test
-  def test_it_exists
+      def test_it_exists
 
-    building = Building.new
+        building = Building.new
 
-    assert_instance_of Building, building
-  end
+        assert_instance_of Building, building
+      end
 
-  def test_it_has_readable_attributes
+      def test_it_has_readable_attributes
 
-    building = Building.new
+        building = Building.new
 
-    assert_equal [], building.units
-    assert_equal [], building.renters
-  end
+        assert_equal [], building.units
+        assert_equal [], building.renters
+      end
 
-    def test_it_can_add_units
-       building = Building.new
-       unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
+      def test_it_can_add_units
+         building = Building.new
+         unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
 
-      assert_equal [unit1], building.add_unit(unit1)
-    end
+        assert_equal [unit1], building.add_unit(unit1)
+      end
 
-    def test_can_add_units_and_renters
+      def test_can_add_units_and_renters
         building = Building.new
         unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
         unit2 = Apartment.new({number: "B2", monthly_rent: 999, bathrooms: 2, bedrooms: 2})
@@ -43,19 +43,16 @@ class BuildingTest < Minitest::Test
       end
 
       def test_can_average_rent
-    building = Building.new
-    unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
-    unit2 = Apartment.new({number: "B2", monthly_rent: 999, bathrooms: 2, bedrooms: 2})
-    building.add_unit(unit1)
-    building.add_unit(unit2)
-    renter1 = Renter.new("Aurora")
-    assert_equal [unit1, unit2], building.units
-    unit1.add_renter(renter1)
-    renter2 = Renter.new("Tim")
-    unit1.add_renter(renter2)
-    assert_equal 1099.5, building.average_rent
-  end
-
-
-
+          building = Building.new
+          unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
+          unit2 = Apartment.new({number: "B2", monthly_rent: 999, bathrooms: 2, bedrooms: 2})
+          building.add_unit(unit1)
+          building.add_unit(unit2)
+          renter1 = Renter.new("Aurora")
+        assert_equal [unit1, unit2], building.units
+          unit1.add_renter(renter1)
+          renter2 = Renter.new("Tim")
+          unit1.add_renter(renter2)
+        assert_equal 1099.5, building.average_rent
+      end
 end
